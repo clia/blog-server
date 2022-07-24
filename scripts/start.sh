c65d34da3792c31ff4d4f92fb72bc37d9a3f1366
clia <clia@163.com>
2022-07-24 14:39:37 +0800

    Fix start.sh.

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,7 +9,7 @@ if [ -e ${dir_root}/server.out ]; then
     mv ${dir_root}/server.out ${dir_root}"/server.out.`date +"%Y%m%d%H%M%S"`"
 fi
 
-RUST_LOG="info,blog_server=trace" nohup ./${filename} > ${dir_root}/server.out 2>&1 &
+RUST_LOG="info,blog_server=trace" nohup ./${filename} clia.tech clia@163.com > ${dir_root}/server.out 2>&1 &
 sleep 1
 ps -eaf | grep "${filename}" | grep -v "grep"
 cd ${old_dir}
